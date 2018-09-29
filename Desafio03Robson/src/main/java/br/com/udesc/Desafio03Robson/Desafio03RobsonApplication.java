9f46e9513f4d4672b5f6d8be49b68b8a3e0532ca
SysadminRS <sestrem.robson@gmail.com>
2018-09-29 18:29:51 -0300

    commit eclipse
    Tentativa para funcionar no heroku

--- Desafio03Robson/src/main/java/br/com/udesc/Desafio03Robson/Desafio03RobsonApplication.java
+++ Desafio03Robson/src/main/java/br/com/udesc/Desafio03Robson/Desafio03RobsonApplication.java
@@ -1,12 +1,12 @@
-package br.com.udesc.DesafioSpring;
+package br.com.udesc.Desafio03Robson;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DesafioSpringApplication {
+public class Desafio03RobsonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DesafioSpringApplication.class, args);
+		SpringApplication.run(Desafio03RobsonApplication.class, args);
 	}
 }
